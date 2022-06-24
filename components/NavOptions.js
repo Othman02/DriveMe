256dccba34bbe949dd57573de9de2ff3c44381b2
Othman02 <107188956+Othman02@users.noreply.github.com>
2022-06-24 21:31:43 +0100

    Add files via upload

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,8 @@
 import { View, Text, FlatList, TouchableOpacity , Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc';
+import { useNavigation } from '@react-navigation/native';
+import { Icon } from '@rneui/base';
 
 const data =[
     {
@@ -12,13 +14,16 @@ const data =[
 ]
 
 const NavOptions = () => {
+    const navigation = useNavigation();
   return (
     <FlatList
     data={data}
-    keyExtractor={(item) => item.id}
     horizontal
+    keyExtractor={(item) => item.id}
     renderItem={({item})=> (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity 
+         onPress={() => navigation.navigate(item.screen) }
+         style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
             <View>
                 <Image
                 style = {{width: 120 , height:120 , resizeMode:"contain"}}
@@ -26,6 +31,12 @@ const NavOptions = () => {
                 />
             </View>
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+                color="white"
+                name='arrowright'
+                type="antdesign"
+                style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+            />
             
         </TouchableOpacity>
     )} 
